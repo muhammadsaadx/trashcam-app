003fec93c9e307e367935ad3dce9bbfaa34970a3
Muhammad Saad Hasan <muhammadsaadhsn@gmail.com>
2025-03-10 21:20:33 +0500

    added and removed dashboard routers

--- src/apis/dashboard/router.py
+++ src/apis/dashboard/router.py
@@ -21,66 +21,14 @@ async def get_all_report_dates():
 
 
 
+@router.get("/get_longitude_latitude")
+async def get_longitude_latitude():
+    query = "SELECT longitude, latitude FROM reports;"
+    result = await Database.read_from_db(query, ())
 
+    if not result:
+            raise HTTPException(status_code=404, detail="No data found")
+        
+    heatmap_data = [[float(point[1]), float(point[0]), 1.0] for point in result]  
+    return heatmap_data
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-@router.get("/get_hot_points")
-async def get_hot_points():
-    query = """
-        SELECT longitude, latitude
-        FROM reports;
-    """
-    
-    result = await Database.read_from_db(query, ())  
-    
-    hot_points = [[point[1], point[0]] for point in result]  
-    
-    return hot_points
-
-
-
-
-@router.get("/get_status_data")
-async def get_status_data():
-    query = """   
-        SELECT 
-            COUNT(reportid) AS total_reports_issued,
-            SUM(CASE WHEN fineStatus = 'Paid' THEN 1 ELSE 0 END) AS paid_reports,
-            SUM(CASE WHEN fineStatus = 'Pending' THEN 1 ELSE 0 END) AS pending_reports,
-            SUM(CASE WHEN fineStatus = 'Missed' THEN 1 ELSE 0 END) AS missed_reports
-        FROM 
-            reports;
-    """
-    
-    result = await Database.read_from_db(query, ())  
-    
-    return result
-    
